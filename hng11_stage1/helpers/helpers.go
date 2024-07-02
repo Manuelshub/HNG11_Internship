19de5f8c5ddeafc29d062e9968c52c0542fccffb
Emmanuel <kcemmy03@gmail.com>
2024-07-02 14:27:49 +0100

    Modified the code

--- hng11_stage1/helpers/helpers.go
+++ hng11_stage1/helpers/helpers.go
@@ -11,15 +11,12 @@ import (
 
 // GetClientIp gets the Ip address of the requester
 func GetClientIp(r *http.Request) string {
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return err.Error()
-	}
-
 	if forwarded := r.Header.Get("X-Forwarded-For"); forwarded != "" {
 		ips := strings.Split(forwarded, ",")
 		return strings.TrimSpace(ips[0])
 	}
+
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ip
 }
 
@@ -43,20 +40,26 @@ func GetClientLocation(clientIp string) string {
 	return result.City
 }
 
-func GetTempByCity(city string) float32 {
+func GetTemperatureByCity(city string) float32 {
 	apiKey := os.Getenv("WEATHER_API_KEY")
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, city)
 
-	Url := fmt.Sprintf("http//api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, city)
-	resp, err := http.Get(Url)
+	resp, err := http.Get(url)
 	if err != nil {
 		return 0
 	}
 	defer resp.Body.Close()
 
-	var data map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+	var weatherData struct {
+		Current struct {
+			Temperature float32 `json:"temp_c"`
+		} `json:"current"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&weatherData)
+	if err != nil {
 		return 0
 	}
 
-	return data["current"].(map[string]interface{})["temp_c"].(float32)
+	return weatherData.Current.Temperature
 }
